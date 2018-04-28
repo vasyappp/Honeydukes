31b3a0e35d25b0291f784d5c438b46e07c6e7969
Natalia Litvinova <tasmantia@gmail.com>
2018-04-28 17:44:54 +0300

    Начало комментариев

--- src/main/java/Candies/BertieBotts.java
+++ src/main/java/Candies/BertieBotts.java
@@ -1,38 +1,79 @@
 package Candies;
 
+/**
+ * Класс БертиБоттс
+ * Описывает продукт вида "Драже Берти Боттс"
+ */
+
 public class BertieBotts extends Candy {
+    /**
+     * Атрибуты класса:
+     * Статическая переменная String fullName - полное название продукта
+     * Статическая переменная String description - краткое описание продукта
+     * Переменная String flavourMix - какие вкусы содержит продукт (три опции: "Every", "Ordinary", "Eeew")
+     */
     private static final String fullName = "Bertie Bott's Every Flavour Beans";
     private static final String description = "Jelly beans with every flavor imaginable";
     private String flavourMix;
 
+
+    /**
+     * Конструктор с заданными параметрами
+     *
+     * @param weight Целочисленное значение веса продукта
+     * @param price Целочисленное значение цены продукта
+     * @param flavourMix Строковое значение набора вкусов
+     */
     public BertieBotts (int weight, int price, String flavourMix) {
         this.weight = weight;
         this.price = price;
         this.flavourMix = flavourMix;
     }
 
+    /**
+     *
+     * @return
+     */
     public static String getFullName() {
         return fullName;
     }
 
+    /**
+     *
+     * @return
+     */
     public static String getDescription() {
         return description;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFlavourMix() {
         return flavourMix;
     }
 
+    /**
+     *
+     * @param flavourMix
+     */
     public void setFlavourMix(String flavourMix) {
         this.flavourMix = flavourMix;
     }
 
+    /**
+     *
+     */
     @Override
     public void print() {
+        //
         System.out.println("Weight of the pack:");
         System.out.println(this.getWeight() + "g");
         System.out.println("Price of the pack:");
         System.out.println(this.getPrice() + " sickles");
+
+        //
         System.out.println("Flavour mix:");
         switch (this.getFlavourMix()) {
             case "Every":
--- src/main/java/Candies/Candy.java
+++ src/main/java/Candies/Candy.java
@@ -1,29 +1,69 @@
 package Candies;
 
+/**
+ * Абстрактный класс Конфета
+ * Содержит вес и цену продукта, а также абстрактный метод вывода информации о продукте
+ *
+ * @author Natalia Litvinova
+ */
+
 public abstract class Candy {
+    /**
+     * Атрибуты метода:
+     * Целое число weight - вес продукта в граммах
+     * Целое число price - цена продукта в сиклях
+     */
     protected int weight;
     protected int price;
 
+
+    /**
+     * Конструктор без заданных параметров
+     */
     public Candy () {
         this.weight = 0;
         this.price = 0;
     }
 
+    /**
+     * Геттер для веса продукта
+     *
+     * @return Целое число - вес продукта
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Сеттер для веса продукта
+     *
+     * @param weight Целое число - вес продукта
+     */
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
+    /**
+     * Геттер для цены продукта
+     *
+     * @return Целое число - цена продукта
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Сеттер для цены продукта
+     *
+     * @param price Целое число - цена продукта
+     */
     public void setPrice(int price) {
         this.price = price;
     }
 
+    /**
+     * Абстрактный метод для вывода информации о продукте
+     * Переопределяется в наследниках
+     */
     public abstract void print();
 }
